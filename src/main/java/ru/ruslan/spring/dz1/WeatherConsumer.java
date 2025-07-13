53845897b23549b976ad352c2745de656f7b40e3
Ulyamaev Ruslan <rulyamaev.2003@mail.ru>
2025-07-13 18:54:11 +0500

    fix

--- src/main/java/ru/ruslan/spring/dz1/WeatherConsumer.java
+++ src/main/java/ru/ruslan/spring/dz1/WeatherConsumer.java
@@ -1,8 +1,5 @@
 package ru.ruslan.spring.dz1;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.kafka.common.protocol.types.Field;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import ru.ruslan.spring.dz1.models.WeatherData;
@@ -10,10 +7,7 @@ import ru.ruslan.spring.dz1.models.WeatherData;
 @Component
 public class WeatherConsumer {
 
-    private static final String topicName = "weather-topic";
-
-
-    @KafkaListener(topics = topicName, groupId = "weather-group")
+    @KafkaListener(topics = "weather-topic", groupId = "weather-group")
     public WeatherData receive(WeatherData message){
         System.out.println("Новая информация по погоде:  " + message.getCity() + " " + message.getCondition() + " +" + message.getTemperature());
         return message;
